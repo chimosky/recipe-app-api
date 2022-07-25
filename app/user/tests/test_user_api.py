34912ed537c1df906956ab10bdfc7a99ba97e333
Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>
2022-07-25 17:59:22 +0100

    Implement token API
    - create token view
    - add support for user validation and authentication
    
    Signed-off-by: Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -92,7 +92,7 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertNotIn(token, res.data)
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
